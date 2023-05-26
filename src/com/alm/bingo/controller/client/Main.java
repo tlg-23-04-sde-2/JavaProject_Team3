b285f01dcc80cb84a31437265a4ea68b0ad5f013
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-05-26 13:04:19 -0600

    added board to the view package

--- src/com/alm/bingo/controller/client/Main.java
+++ src/com/alm/bingo/controller/client/Main.java
@@ -1,15 +1,6 @@
 package com.alm.bingo.controller.client;
 
 
-import com.alm.bingo.controller.Board;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 class Main {
 
     public static void main(String[] args) {
