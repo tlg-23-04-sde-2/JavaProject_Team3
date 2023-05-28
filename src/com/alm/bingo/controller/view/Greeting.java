9ce01a28d8bc1721e10891e8b60c2a2dbabfaf39
Lilly <brittany.lilly@TLGcohort.com>
2023-05-28 05:42:41 -0800

    Lilly Art

--- src/com/alm/bingo/controller/view/Greeting.java
+++ src/com/alm/bingo/controller/view/Greeting.java
@@ -1,11 +1,13 @@
 package com.alm.bingo.controller.view;
 
-class Welcome {
-    String greeting = "ALM BINGO NOT-INCORPORATED\n" +
+class Greeting {
+    private static String greeting = "ALM BINGO NOT-INCORPORATED\n" +
             "(\\_/) \n" +
             "(0.0) \n" +
             "(m m)o \n" +
             "CONGRATULATIONS YOU WIN!!!!!!!";
-
+private void welcome() {
+    System.out.println(greeting);
+}
 
 }   // END OF CLASS
